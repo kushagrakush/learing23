37ee96cbd653653384e3cda375c0cc9f23831269
kushagrakush <kushagra.shukla@impressico.com>
2023-01-23 18:50:02 +0530

    committed

---
+++ src/main/java/com/springBootOneToOneMap/SpringBootHybernetOneToOneMapApplication.java
@@ -0,0 +1,13 @@
+package com.springBootOneToOneMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHybernetOneToOneMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHybernetOneToOneMapApplication.class, args);
+	}
+
+}
